a035a1e0ab302534d0a3b27d974476d79ca18607
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-04 14:55:33 -0500

    Dynamically creating blog pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges{
+                    node{
+                        frontmatter {
+                            title
+                            date
+                        },
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Posts will show here</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge) => {
+                    return (
+                        <li>
+                            <h2><Link to={`/blog/${edge.node.fields.slug}`}>{edge.node.frontmatter.title}</Link></h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
