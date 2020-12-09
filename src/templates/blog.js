f5096bb02035e393136a932cc2d31e37327e27bd
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-08 19:06:36 -0500

    Functioning Contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Layout from '../components/layout'
 
 export const query = graphql`
@@ -7,17 +8,31 @@ export const query = graphql`
     contentfulBlogPost(slug: {eq: $slug}) {
       title
       publishDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
 
 const Blog = (props) => {
-    return (
-        <Layout>
-          <h1>{props.data.contentfulBlogPost.title}</h1>
-          <p>{props.data.contentfulBlogPost.publishDate}</p>
-        </Layout>
-    )
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title['en-US'];
+        const url = node.data.target.fields.file['en-US'].url;
+        return <img alt={alt} src={url} />
+      }
+    }
+  } 
+
+  return (
+      <Layout>
+        <h1>{props.data.contentfulBlogPost.title}</h1>
+        <p>{props.data.contentfulBlogPost.publishDate}</p>
+        {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+      </Layout>
+  )
 }
 
 export default Blog
